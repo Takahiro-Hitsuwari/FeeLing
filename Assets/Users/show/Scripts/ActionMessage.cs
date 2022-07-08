a9be02116939d78d295b3da80b8ec0272ff2deeb
DESKTOP-8CRU2DJ\PC_User <show20010521@gmail.com>
2022-07-08 18:25:48 +0900

    コメント追加

--- Assets/Users/show/Scripts/ActionMessage.cs
+++ Assets/Users/show/Scripts/ActionMessage.cs
@@ -5,8 +5,10 @@ using UnityEngine.UI;
 
 public class ActionMessage : MonoBehaviour
 {
+    //イメージ
     [SerializeField]
     GameObject ActionButton;
+    //テキスト
     [SerializeField]
     Text ActionText;
     // Start is called before the first frame update
@@ -23,18 +25,19 @@ public class ActionMessage : MonoBehaviour
 
     private void OnTriggerStay(Collider collider)
     {
-        if (collider.gameObject.name == "Door")
+        //一定の範囲に入ったらボタンイメージとテキストを表示
+        if (collider.gameObject.name == "Door")//ドアの時
         {
             ActionButton.SetActive(true);
             ActionText.text = "入る";
         }
-        else if(collider.gameObject.name == "Guide")
+        else if(collider.gameObject.name == "Guide")//案内人
         {
             ActionButton.SetActive(true);
             ActionText.text = "話す";
         }
     }
-
+    //抜けたら表示を切る
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject)

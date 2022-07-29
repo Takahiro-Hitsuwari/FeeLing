f65c835375f392aeec605cd638d2e364a4034399
Takahiro_Hitsuwari <ta12ka04ro16@gmail.com>
2022-07-29 15:18:07 +0900

    最新データ更新

--- Assets/Scripts/RetryManager.cs
+++ Assets/Scripts/RetryManager.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
 public class RetryManager : MonoBehaviour
 {
     LevelLoader levload;
+    public GameObject continueButton;
+    EventSystem evntSys;
 
+ void Awake() {
+     evntSys = EventSystem.current;
+    
+}
     private void Start()
     {
+        evntSys.SetSelectedGameObject(null);
+        evntSys.SetSelectedGameObject(continueButton);
         levload = GameObject.Find("LevelLoader").GetComponent<LevelLoader>();
     }
 
